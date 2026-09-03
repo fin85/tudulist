16c8f47871c1d23a3263c169d11d07b152199d9f
fin85 <menonuds@gmail.com>
2026-09-04 01:31:51 +0400

    Route frontend through Vercel API proxy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TUDU_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzH2z96CgX1m4SQo8tD6nLPMo78mwvEd_JypngRnvPEslNunI52mJgZRlxl8AUmbZ1e/exec",
+  apiUrl: "/api/tasks",
   timezone: "Asia/Yerevan",
   refreshMinutes: 60
 };
