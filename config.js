bb355c7e4ebbd3bbc7e7ab2d2987e65f0daa16d5
fin85 <menonuds@gmail.com>
2026-09-04 01:30:36 +0400

    Connect Google Apps Script API

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// После публикации Google Apps Script вставь URL веб-приложения сюда.
 window.TUDU_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzH2z96CgX1m4SQo8tD6nLPMo78mwvEd_JypngRnvPEslNunI52mJgZRlxl8AUmbZ1e/exec",
   timezone: "Asia/Yerevan",
   refreshMinutes: 60
 };
