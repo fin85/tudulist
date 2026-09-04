9d7187cff9ef1fa6a4ec85fc485cff907c11668f
fin85 <menonuds@gmail.com>
2026-09-04 14:04:34 +0400

    Add VAPID public key for Web Push

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.TUDU_CONFIG = {
   apiUrl: "/api/tasks",
   timezone: "Asia/Yerevan",
-  refreshMinutes: 60
+  refreshMinutes: 60,
+  vapidPublicKey: "BJufjcw108ZxDOiuHbrHgUioNEXQ3ZDANiLfkGDtfxAwt1uK9m3qB0F43qWEVlGul6AXXir-pUUwNFG5Of0SaQo"
 };
